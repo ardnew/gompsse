b55b4cf1d22a4452456554dbde55f8ede727142c
ardnew <andrew@ardnew.com>
2020-02-18 00:52:18 -0600

    add SPI config init

--- gompsse.go
+++ gompsse.go
@@ -1,59 +1,50 @@
 package gompsse
 
-// #cgo darwin,amd64 LDFLAGS: -framework CoreFoundation -framework IOKit
-// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/native/darwin_amd64/lib
-// #cgo darwin,amd64  CFLAGS: -I${SRCDIR}/native/darwin_amd64/inc
-// #cgo linux,386    LDFLAGS: -L${SRCDIR}/native/linux_386/lib
-// #cgo linux,386     CFLAGS: -I${SRCDIR}/native/linux_386/inc
-// #cgo linux,amd64  LDFLAGS: -L${SRCDIR}/native/linux_amd64/lib
-// #cgo linux,amd64   CFLAGS: -I${SRCDIR}/native/linux_amd64/inc
-// #cgo linux,arm64  LDFLAGS: -L${SRCDIR}/native/linux_arm64/lib
-// #cgo linux,arm64   CFLAGS: -I${SRCDIR}/native/linux_arm64/inc
-// #cgo              LDFLAGS: -lMPSSE
-// #include "ftd2xx.h"
-import "C"
-
-type FTHandle C.FT_HANDLE
-type FTStatus C.FT_STATUS
-
-type FTError int
-
-// Constants related to device status
-const (
-	FTOK                      FTError = C.FT_OK
-	FTInvalidHandle           FTError = C.FT_INVALID_HANDLE
-	FTDeviceNotFound          FTError = C.FT_DEVICE_NOT_FOUND
-	FTDeviceNotOpened         FTError = C.FT_DEVICE_NOT_OPENED
-	FTIOError                 FTError = C.FT_IO_ERROR
-	FTInsufficientResources   FTError = C.FT_INSUFFICIENT_RESOURCES
-	FTInvalidParameter        FTError = C.FT_INVALID_PARAMETER
-	FTInvalidBaudRate         FTError = C.FT_INVALID_BAUD_RATE
-	FTDeviceNotOpenedForErase FTError = C.FT_DEVICE_NOT_OPENED_FOR_ERASE
-	FTDeviceNotOpenedForWrite FTError = C.FT_DEVICE_NOT_OPENED_FOR_WRITE
-	FTFailedToWriteDevice     FTError = C.FT_FAILED_TO_WRITE_DEVICE
-	FTEEPROMReadFailed        FTError = C.FT_EEPROM_READ_FAILED
-	FTEEPROMWriteFailed       FTError = C.FT_EEPROM_WRITE_FAILED
-	FTEEPROMEraseFailed       FTError = C.FT_EEPROM_ERASE_FAILED
-	FTEEPROMNotPresent        FTError = C.FT_EEPROM_NOT_PRESENT
-	FTEEPROMNotProgrammed     FTError = C.FT_EEPROM_NOT_PROGRAMMED
-	FTInvalidArgs             FTError = C.FT_INVALID_ARGS
-	FTNotSupported            FTError = C.FT_NOT_SUPPORTED
-	FTOtherError              FTError = C.FT_OTHER_ERROR
-	FTDeviceListNotReady      FTError = C.FT_DEVICE_LIST_NOT_READY
-)
-
-// Constants related to GPIO pin configuration
-const (
-	PIN_LO byte = 0 // pin value clear
-	PIN_HI byte = 1 // pin value set
-	PIN_IN byte = 0 // pin direction input
-	PIN_OT byte = 1 // pin direction output
-)
-
 type MPSSE struct {
 	I2C *I2C
 	SPI *SPI
 }
+
+// Types representing individual port pins.
+type (
+	DPin byte // pin on MPSSE low-byte lines (port "D" on FT232H)
+	CPin byte // pin on MPSSE high-byte lines (port "C" on FT232H)
+)
+
+// Constants related to GPIO pin configuration
+const (
+	PinLO byte = 0 // pin value clear
+	PinHI byte = 1 // pin value set
+	PinIN byte = 0 // pin direction input
+	PinOT byte = 1 // pin direction output
+
+	NumDPins = 8 // number of MPSSE low-byte line pins
+	NumCPins = 8 // number of MPSSE high-byte line pins
+)
+
+// Constants defining the available board pins on MPSSE low-byte lines
+const (
+	D0 DPin = 1 << iota
+	D1
+	D2
+	D3
+	D4
+	D5
+	D6
+	D7
+)
+
+// Constants defining the available board pins on MPSSE high-byte lines
+const (
+	C0 CPin = 1 << iota
+	C1
+	C2
+	C3
+	C4
+	C5
+	C6
+	C7
+)
 
 func NewMPSSE() *MPSSE {
 	return &MPSSE{I2C: nil, SPI: nil}
--- gompsse_spi.go
+++ gompsse_spi.go
@@ -3,93 +3,116 @@ package gompsse
 // #include "libMPSSE_spi.h"
 import "C"
 
+// spiPinConfig represents the default direction and value for pins associated
+// with the lower byte lines of MPSSE, reserved for serial functions SPI/I²C
+// (or port "D" on FT232H)
+type spiPinConfig struct {
+	initDir  byte // direction of lines after SPI channel initialization
+	initVal  byte // value of lines after SPI channel initialization
+	closeDir byte // direction of lines after SPI channel is closed
+	closeVal byte // value of lines after SPI channel is closed
+}
+
 // Constants controlling the supported SPI transfer options
 const (
-	SPITransferOptionsSizeInBytes       = 0x00000000 // size is provided in bytes
-	SPITransferOptionsSizeInBits        = 0x00000001 // size is provided in bits
-	SPITransferOptionsChipselectEnable  = 0x00000002 // assert CS before start
-	SPITransferOptionsChipselectDisable = 0x00000004 // deassert CS after end
+	spiXferBytes = 0x00000000 // size is provided in bytes
+	spiXferBits  = 0x00000001 // size is provided in bits
+	spiCSEnable  = 0x00000002 // assert CS before start
+	spiCSDisable = 0x00000004 // deassert CS after end
 )
 
-const SPIClockRateDefault = 100000 // valid range: 0-30000000 (30 MHz)
-const SPILatencyTimer = value      // 1-255 USB Hi-Speed, 2-255 USB Full-Speed
-
-// Constants defining the supported SPI modes.
-//   NOTE: the libMPSSE engine only supports mode 0 and mode 2 (CPHA==2).
+// Constants defining the available options in the SPI configuration struct.
 const (
-	SPIConfigOptionModeMask = 0x00000003
-	SPIConfigOptionMode0    = 0x00000000 // capture on RISE, propagate on FALL
-	SPIConfigOptionMode1    = 0x00000001 // capture on FALL, propagate on RISE
-	SPIConfigOptionMode2    = 0x00000002 // capture on FALL, propagate on RISE
-	SPIConfigOptionMode3    = 0x00000003 // capture on RISE, propagate on FALL
+	// Known SPI operating modes
+	//   LIMITATION: libMPSSE only supports mode 0 and mode 2 (CPHA==2).
+	SPIMode0       = 0x00000000 // capture on RISE, propagate on FALL
+	SPIMode1       = 0x00000001 // capture on FALL, propagate on RISE
+	SPIMode2       = 0x00000002 // capture on FALL, propagate on RISE
+	SPIMode3       = 0x00000003 // capture on RISE, propagate on FALL
+	spiModeMask    = 0x00000003
+	spiModeInvalid = 0x000000FF
+	spiModeDefault = SPIMode0
 
-	SPIConfigOptionModeDefault = SPIConfigOptionMode0
+	// DPins available for chip-select operation
+	spiCSD3      = 0x00000000 // SPI CS on D3
+	spiCSD4      = 0x00000004 // SPI CS on D4
+	spiCSD5      = 0x00000008 // SPI CS on D5
+	spiCSD6      = 0x0000000C // SPI CS on D6
+	spiCSD7      = 0x00000010 // SPI CS on D7
+	spiCSMask    = 0x0000001C
+	spiCSInvalid = 0x000000FF
+	spiCSDefault = spiCSD3
+
+	// Other options
+	spiCSActiveLow  = 0x00000020 // drive pin low to assert CS
+	spiCSActiveHigh = 0x00000000 // drive pin high to assert CS
 )
 
-// Constants defining the supported chip-select pins and options
-const (
-	SPIConfigOptionCSMask  = 0x0000001C
-	SPIConfigOptionCSDBUS3 = 0x00000000 // CS on D3
-	SPIConfigOptionCSDBUS4 = 0x00000004 // CS on D4
-	SPIConfigOptionCSDBUS5 = 0x00000008 // CS on D5
-	SPIConfigOptionCSDBUS6 = 0x0000000C // CS on D6
-	SPIConfigOptionCSDBUS7 = 0x00000010 // CS on D7
-
-	SPIConfigOptionsCSDefault = SPIConfigOptionCSDBUS3
-
-	SPIConfigOptionCSActivelow = 0x00000020 // drive pin low to assert CS
-)
+// SPICSPin translates a DPin value to its corresponding chip-select mask for
+// the SPI configuration struct option.
+var SPICSPin = map[DPin]uint32{
+	D0: spiCSInvalid,
+	D1: spiCSInvalid,
+	D2: spiCSInvalid,
+	D3: spiCSD3,
+	D4: spiCSD4,
+	D5: spiCSD5,
+	D6: spiCSD6,
+	D7: spiCSD7,
+}
 
 // Constants related to board pins when MPSSE operating in SPI mode
 const (
-	DBUSNumPins = 8
-	CBUSNumPins = 8
+	spiClockRateDefault = 100000 // valid range: 0-30000000 (30 MHz)
+	spiLatencyDefault   = 16     // 1-255 USB Hi-Speed, 2-255 USB Full-Speed
 )
 
-// SPIPinConfig represents the default direction and value for pins associated
-// with the lower byte lines of MPSSE, reserved for serial functions SPI/I²C
-// (or port "D" on FT232H)
-type SPIPinConfig struct {
-	InitDir  byte // direction of lines after SPI channel initialization
-	InitVal  byte // value of lines after SPI channel initialization
-	CloseDir byte // direction of lines after SPI channel is closed
-	CloseVal byte // value of lines after SPI channel is closed
-}
-
-// DefaultSPIPinConfig defines the initial SPIPinConfig value for all pins
+// defaultspiPinConfig defines the initial spiPinConfig value for all pins
 // represented by this type.
-var DefaultSPIPinConfig = [DBUSNumPins]*SPIPinConfig{
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_LO, CloseDir: PIN_OT, CloseVal: PIN_LO}, // D0 SCLK
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_LO, CloseDir: PIN_OT, CloseVal: PIN_LO}, // D1 MOSI
-	&SPIPinConfig{InitDir: PIN_IN, InitVal: PIN_LO, CloseDir: PIN_IN, CloseVal: PIN_LO}, // D2 MISO
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_HI, CloseDir: PIN_OT, CloseVal: PIN_HI}, // D3 CS
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_LO, CloseDir: PIN_OT, CloseVal: PIN_LO}, // D4 GPIO
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_LO, CloseDir: PIN_OT, CloseVal: PIN_LO}, // D5 GPIO
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_LO, CloseDir: PIN_OT, CloseVal: PIN_LO}, // D6 GPIO
-	&SPIPinConfig{InitDir: PIN_OT, InitVal: PIN_LO, CloseDir: PIN_OT, CloseVal: PIN_LO}, // D7 GPIO
+var spiPinConfigDefault = [NumDPins]*spiPinConfig{
+	&spiPinConfig{initDir: PinOT, initVal: PinLO, closeDir: PinOT, closeVal: PinLO}, // D0 SCLK
+	&spiPinConfig{initDir: PinOT, initVal: PinLO, closeDir: PinOT, closeVal: PinLO}, // D1 MOSI
+	&spiPinConfig{initDir: PinIN, initVal: PinLO, closeDir: PinIN, closeVal: PinLO}, // D2 MISO
+	&spiPinConfig{initDir: PinOT, initVal: PinHI, closeDir: PinOT, closeVal: PinHI}, // D3 CS
+	&spiPinConfig{initDir: PinOT, initVal: PinLO, closeDir: PinOT, closeVal: PinLO}, // D4 GPIO
+	&spiPinConfig{initDir: PinOT, initVal: PinLO, closeDir: PinOT, closeVal: PinLO}, // D5 GPIO
+	&spiPinConfig{initDir: PinOT, initVal: PinLO, closeDir: PinOT, closeVal: PinLO}, // D6 GPIO
+	&spiPinConfig{initDir: PinOT, initVal: PinLO, closeDir: PinOT, closeVal: PinLO}, // D7 GPIO
 }
 
-// SPIConfig holds all of the configuration settings for an SPI channel.
-type SPIConfig struct {
-	ClockRate uint32 // in Hertz
-	Latency   uint8  // in ms
-	Options   uint32
-	Pin       uint32
-	Reserved  uint16
+// spiConfig holds all of the configuration settings for an SPI channel.
+type spiConfig struct {
+	clockRate uint32 // in Hertz
+	latency   uint8  // in ms
+	options   uint32
+	pin       uint32
+	reserved  uint16
 }
 
-// SetPin constructs the 32-bit Pin field of the SPIConfig struct from the
-// provided SPIPinConfig slice cfg for each pin (identified by its index in the
+// SetPin constructs the 32-bit pin field of the spiConfig struct from the
+// provided spiPinConfig slice cfg for each pin (identified by its index in the
 // given slice).
-func (sc *SPIConfig) SetPin(cfg []*SPIPinConfig) {
+func (sc *spiConfig) setPin(cfg [NumDPins]*spiPinConfig) {
 
-	sc.Pin = 0
+	sc.pin = 0
 	for i, c := range cfg {
-		sc.Pin |= (uint32(c.InitDir) << i) | (uint32(c.InitVal) << (8 + i)) |
-			(uint32(c.CloseDir) << (16 + i)) | (uint32(c.CloseVal) << (24 + i))
+		sc.pin |= (uint32(c.initDir) << i) | (uint32(c.initVal) << (8 + i)) |
+			(uint32(c.closeDir) << (16 + i)) | (uint32(c.closeVal) << (24 + i))
 	}
 }
 
 type SPI struct {
-	Config SPIConfig
+	config *spiConfig
+}
+
+func NewSPI(cs DPin) (*SPI, error) {
+
+	config := &spiConfig{
+		clockRate: spiClockRateDefault,
+		latency:   spiLatencyDefault,
+		options:   spiCSActiveLow | spiCSDefault | spiModeDefault,
+	}
+	config.setPin(spiPinConfigDefault)
+
+	return &SPI{config: config}, nil
 }
